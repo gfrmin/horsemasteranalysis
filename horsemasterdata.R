a67c93cb09b058a696ecc98149b9d9372e8faa95
Guy Freeman <git@gfrm.in>
2016-09-22 21:56:14 +0800

    From 2007/2008 season

--- horsemasterdata.R
+++ horsemasterdata.R
@@ -8,8 +8,9 @@ source("secretconnect.R")
 # load data and model
 
 performances <- db.data.frame("public.performances")
-performances <- performances[performances$race_date > "2006-01-01"]
-performancesdt <- lk(performances, -1)
+performances <- performances[performances$race_date >= "2007-09-09"]
+performances <- performances[,c("horse_id", "horse_no", "race_no", "race_date", "season", "going", "rating", "jockey_id", "trainer_id", "winning_odds", "actual_weight", "on_date_weight", "gears", "draw", "race_class", "distance", "course", "race_location", "finish_time")]
+performancesdt <- lk(performances, 10)
 
 # finish
 
