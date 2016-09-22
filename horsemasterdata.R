49dc425ab7b6a46cd1c1397168208e91a4e4b2ff
Guy Freeman <git@gfrm.in>
2016-09-22 22:24:57 +0800

    Save partial data locally

--- horsemasterdata.R
+++ horsemasterdata.R
@@ -10,7 +10,9 @@ source("secretconnect.R")
 performances <- db.data.frame("public.performances")
 performances <- performances[performances$race_date >= "2007-09-09"]
 performances <- performances[,c("horse_id", "horse_no", "race_no", "race_date", "season", "going", "rating", "jockey_id", "trainer_id", "winning_odds", "actual_weight", "on_date_weight", "gears", "draw", "race_class", "distance", "course", "race_location", "finish_time")]
-performancesdt <- lk(performances, 10)
+performancesdt <- data.table(lk(performances, -1))
+
+write.csv(performancesdt, file = "performancesdt.csv", row.names = FALSE)
 
 # finish
 
