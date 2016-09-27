71a78c7e65cd1acf2d6fa65e86a39a731f191c89
Guy <c9@gfrm.in>
2016-09-27 03:30:20 +0000

    Download all data, around 104MB

--- horsemasterdata.R
+++ horsemasterdata.R
@@ -8,8 +8,6 @@ source("secretconnect.R")
 # load data and model
 
 performances <- db.data.frame("public.performances")
-performances <- performances[performances$race_date >= "2007-09-09"]
-performances <- performances[,c("horse_id", "horse_no", "race_no", "race_date", "season", "going", "rating", "jockey_id", "trainer_id", "winning_odds", "actual_weight", "on_date_weight", "gears", "draw", "race_class", "distance", "course", "race_location", "finish_time")]
 performancesdt <- data.table(lk(performances, -1))
 
 write.csv(performancesdt, file = "performancesdt.csv", row.names = FALSE)
