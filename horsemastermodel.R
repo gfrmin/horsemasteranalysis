aa1bcd98b269777c6b46a33c7b77f8e399210654
Guy <c9@gfrm.in>
2016-09-30 02:35:25 +0000

    Started modelling

---
+++ horsemastermodel.R
@@ -0,0 +1,10 @@
+library(data.table)
+
+performancesdt <- fread("performancesdt.csv")
+performancesdt <- performancesdt[,list(horse_id, horse_no, race_no, race_date, race_class, going, rating, jockey_name, trainer_name, winning_odds, actual_weight, on_date_weight, gears, draw, race_class, distance, course, race_location, track)]
+
+library(stringr)
+
+library(lubridate)
+performancesdt[,race_date := ymd(race_date)]
+performancesdt[,`:=`(raceday = day(race_date), racemonth = month(race_date), raceyear = year(race_date))]
