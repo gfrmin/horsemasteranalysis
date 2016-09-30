c0743e17ee9191a340810ed900187307d022d779
Guy Freeman <git@gfrm.in>
2016-10-01 01:36:11 +0800

    Ready to train model, really

--- horsemasterclean.R
+++ horsemasterclean.R
@@ -10,8 +10,8 @@ performancesdt[,race_date := ymd(race_date)]
 performancesdt[,`:=`(raceday = day(race_date), racemonth = month(race_date), raceyear = year(race_date))]
 performancesdt <- performancesdt[raceyear >= 2000] # don't need all races!
 
-# actual weight should be above 0
-performancesdt <- performancesdt[actual_weight > 0]
+# actual weight should be above 0 and have an on_date_weight
+performancesdt <- performancesdt[actual_weight > 0][!is.na(on_date_weight)]
 
 # rating should be numeric?
 performancesdt[,rating := as.numeric(rating)]
@@ -25,6 +25,7 @@ finishtimes <- finishtimes[,lapply(.SD, as.numeric)]
 finishtimes[,V1 := V1*60]
 finishtimes[,finish_time := V1 + V2]
 performancesdt[,finish_time := finishtimes$finish_time]
+rm(finishtimes)
 
 # save clean(er) dataset
 
--- horsemastermodel.R
+++ horsemastermodel.R
@@ -1,2 +1,18 @@
 library(data.table)
 library(caret)
+
+performancesdt <- fread("performancesdtclean.csv")
+performancemodel <- performancesdt[,list(race_no,going,rating,winning_odds,actual_weight,on_date_weight,draw,race_class,distance,course,race_location,track,raceday,racemonth,raceyear,finish_time)]
+
+performancemodel <- performancemodel[!is.na(rating)]
+
+trainset <- performancemodel[raceyear != 2016]
+testset <- performancemodel[raceyear == 2016]
+
+fitControl <- trainControl(
+  method = "oob", verboseIter = TRUE
+  )
+
+rffit <- train(finish_time ~ ., data = trainset, method = "rf", trControl = fitControl, tuneGrid = data.frame(mtry = c(2,6,10,14,17)))
+
+
