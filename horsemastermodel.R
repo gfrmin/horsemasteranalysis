9794ba58a9e4190eb7d54d6847cb1919083b09fd
Guy Freeman <git@gfrm.in>
2016-10-03 02:04:03 +0800

    Prove profit!

--- horsemastermodel.R
+++ horsemastermodel.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(caret)
 
-performancesdt <- fread("performancesdtclean.csv")
+performancesdt <- fread("performancesclean.csv")
 performancemodel <- performancesdt[,list(race_no,going,rating,winning_odds,actual_weight,on_date_weight,draw,race_class,distance,course,race_location,track,raceday,racemonth,raceyear,finish_time)]
 
 performancemodel <- performancemodel[!is.na(rating)]
@@ -17,8 +17,19 @@ rffit <- train(finish_time ~ ., data = trainset, method = "rf", trControl = fitC
 
 trainpredict <- predict(rffit, trainset)
 testpredict <- predict(rffit, testset)
-trainset$pred <- trainpredict
-testset$pred <- testpredict
+trainset$predtime <- trainpredict
+testset$predtime <- testpredict
 
 write.csv(trainset, file = "trainset.csv", row.names = FALSE)
 write.csv(testset, file = "testset.csv", row.names = FALSE)
+
+trainset[,predrank := rank(predtime, ties.method = "random"), by=list(race_no, raceday, racemonth, raceyear)]
+testset[,predrank := rank(predtime, ties.method = "random"), by=list(race_no, raceday, racemonth, raceyear)]
+
+trainmore <- merge(trainset, performancesdt, by = intersect(names(trainset), names(performancesdt)), all.x = TRUE)
+testmore <- merge(testset, performancesdt, by = intersect(names(testset), names(performancesdt)), all.x = TRUE)
+
+# betting strategy... profit?
+testbet <- testmore[predrank == 1]
+testbet[,profit := ifelse(final_placing == 1 & !is.na(final_placing), winning_odds, -1)]
+testtotalprofit <- testbet[,sum(profit)] # PROFIT!
